09c8114007df286f1bab30ab21afb85de008e0c3
Yisaaaa <ryansanisit19@gmail.com>
2025-08-08 09:10:32 +0800

    Added function and test for creating qcow2 disk file

--- src/worker_node/core/qemu_controller.py
+++ src/worker_node/core/qemu_controller.py
@@ -1,3 +1,34 @@
+import subprocess
+from pathlib import Path
 
 class QemuController:
-    ...
+
+	@classmethod
+	def create_qcow2_img(cls, path: Path, size_mb: int) -> None:
+		""" 
+		Creates a qcow2 disk file for installing alpine linux into.
+
+		Args:
+			img_name (str): name of the qcow2 file
+			size (int): size of the disk file in Megabytes
+		"""
+
+		if size_mb < 50:
+			raise ValueError("qcow2 size must atleast be 50 Mb")
+
+		if path.exists() and path.is_dir():
+			raise ValueError(f"Provided path '{path}' is a dir")
+
+		if path.suffix != ".qcow2":
+			path = path.with_suffix(".qcow2")
+
+
+		try:
+			subprocess.run(["qemu-img", "create", "-f", "qcow2", str(path), f"{size_mb}M"],
+				   check=True)
+			print(f"Successfully created {path} disk file")
+			
+		except subprocess.CalledProcessError as e:
+			print("Failed to create qcow2 image. \n")
+			print(f"Command output: \n{e}")
+			raise
---
+++ test/test_qemu_controller.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from worker_node.core.qemu_controller import QemuController
+
+# tmp_path is a built in fixture by pytest that provides temproray path
+# this path gets automatically cleaned up after running the test
+
+def test_create_qcow2_img(tmp_path: Path) -> None:
+	img_path = tmp_path / "test_disk.qcow2"
+
+	QemuController.create_qcow2_img(img_path, 64)
+
+	assert img_path.exists()
