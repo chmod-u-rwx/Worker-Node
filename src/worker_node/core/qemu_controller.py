71a792418e058ea076c0ccb82976651363aa3152
0CottonBuds <emiljohnllanes772@gmail.com>
2025-08-08 12:06:29 +0800

    Added function stubs in qemu_controller.py

--- src/worker_node/core/qemu_controller.py
+++ src/worker_node/core/qemu_controller.py
@@ -1,3 +1,24 @@
 
+from pathlib import Path
+
+
 class QemuController:
-    ...
+    def __init__(self, img_path: Path, cpu_count: int = 1, memory_allocated: int = 0) -> None:
+        self.img_path = img_path 
+        self.cpu_coun = cpu_count
+        self.memory_allocated = memory_allocated
+
+    def start(self):
+        ...
+    
+    def stop(self):
+        ...
+
+    def reset(self) -> None:
+        ...
+    
+    def run_command(self):
+        ...
+    
+    def get_status(self):
+        ...
