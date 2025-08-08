eed27fbd525e724ede47b56fc5c42763a37603dd
0CottonBuds <emiljohnllanes772@gmail.com>
2025-08-08 16:23:09 +0800

    Added QemuPool Startup sequence

--- src/worker_node/core/qemu_pool.py
+++ src/worker_node/core/qemu_pool.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 from .qemu_controller import QemuController
+from ..config import MAX_CPU_COUNT_ALLOCATED, MAX_MEMORY_ALLOCATED
 
 class QemuPoolEmptyError(Exception):
     pass
@@ -7,7 +9,9 @@ class QemuPool:
     def __init__(self) -> None:
         self.warm_queue: list[QemuController] = []
         self.running_queue: list[QemuController] = []
-    
+
+        self._warm_vms()
+   
     def get_memory_usage(self) -> int:
         ...
     
@@ -22,6 +26,13 @@ class QemuPool:
     
     def cleanup(self) -> None:
         ...
-    
+
+    def _warm_vms(self):
+        memory_per_machine = MAX_MEMORY_ALLOCATED // MAX_CPU_COUNT_ALLOCATED
+        for _ in range(MAX_CPU_COUNT_ALLOCATED):
+            path = Path("./test/path") # update this
+            qemu = QemuController(path, 1, memory_per_machine)
+            self.warm_queue.append(qemu)
+     
 
 qemu_pool = QemuPool()
--- test/test_qemu_pool.py
+++ test/test_qemu_pool.py
@@ -9,7 +9,7 @@ def test_qemu_creating_new_vm_at_startup():
     assert len(pool.warm_queue) == MAX_CPU_COUNT_ALLOCATED, "Qemu pool did not create expected numbers of warm vms"
     expected_memory_allocated = (MAX_MEMORY_ALLOCATED // MAX_CPU_COUNT_ALLOCATED)
     drift = pool.warm_queue[0].memory_allocated - expected_memory_allocated 
-    assert drift > 100, "Memory allocated was not the expected value"
+    assert drift < 100, "Memory allocated was not the expected value"
 
 def test_qemu_acquire_release():
     pool = QemuPool()
