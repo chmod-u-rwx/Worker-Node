54db9a5c488cd3fe28d236ed6372f94e4e163cc8
Yisaaaa <ryansanisit19@gmail.com>
2025-08-09 08:45:58 +0800

    Removed code for setting alpine linux

--- src/worker_node/core/qemu_controller.py
+++ src/worker_node/core/qemu_controller.py
@@ -1,5 +1,4 @@
 import subprocess
-import pexpect
 from pathlib import Path
 from enum import Enum
 
@@ -12,7 +11,8 @@ class QemuStatus(Enum):
 class QemuController:
     def __init__(self, img_path: Path, cpu_count: int = 1, memory_allocated: int = 0) -> None:
         self.img_path = img_path 
-        self.cpu_coun = cpu_count
+        self.cpu_count = cpu_count
+        self.snapshot_name = "base"
         self.memory_allocated = memory_allocated
         self.status = QemuStatus.STARTED
 
@@ -31,157 +31,39 @@ class QemuController:
     def get_status(self):
         ...
 
-    def create_qcow2_img(self, path: Path, size_mb: int) -> None:
-        """ 
-        Creates a qcow2 disk file for installing alpine linux into.
 
-        Args:
-            img_name (str): name of the qcow2 file
-            size (int): size of the disk file in Megabytes
-        """
-
-        if size_mb < 50:
-            raise ValueError("qcow2 size must atleast be 50 Mb")
-
-        if path.exists() and path.is_dir():
-            raise ValueError(f"Provided path '{path}' is a dir")
-
-        if path.suffix != ".qcow2":
-            path = path.with_suffix(".qcow2")
-
-
-        try:
-            subprocess.run(["qemu-img", "create", "-f", "qcow2", str(path), f"{size_mb}M"],
-                   check=True)
-            print(f"Successfully created {path} disk file")
-            
-        except subprocess.CalledProcessError as e:
-            print("Failed to create qcow2 image. \n")
-            print(f"Command output: \n{e}")
-            raise
-
-
-    def setup_alpine_linux(self, qcow2_file: Path, alpine_iso: Path, virtualization: str):
-        """
-        Setups up and install alpine linux on qcow2_file.
-
-        Args:
-            qcow2_file (Path)
-            virtualization (str): May be 'macos' or 'linux' (for now)
-        """
-
-        self._validate_setup_args(qcow2_file, alpine_iso, virtualization)
-
-        cmd = QemuController._get_qemu_cmd(qcow2_file, alpine_iso, virtualization)
-
-        child = pexpect.spawn(cmd, timeout=35)		 # type: ignore
-
-        # setup-alpine sequential prompts
-        prompts_and_responses = QemuController._get_alpine_setup_prompts()
-
-        for prompt, response in prompts_and_responses:
-            try:
-                child.expect(prompt)
-                child.sendline(response)
-            except pexpect.TIMEOUT:
-                print(f"Timeout waiting for: {prompt}")
-                raise
-            except pexpect.EOF:
-                print("Qemu process ended unexpectedly.")
-                raise
-        else:
-            try:
-                child.expect("Installation is complete")
-                print("Installation completed successfully.")
-            except pexpect.TIMEOUT:
-                raise Exception("Installation did not complete in time.")
-            except pexpect.EOF:
-                raise Exception("QEMU process ended before installation finished.")
-        
-
-    @staticmethod
-    def _get_qemu_cmd(qcow2_file: Path, alpine_iso: Path, virtualization: str) -> str:
+    def _get_qemu_cmd(self, virtualization: str, loadvm: bool = False) -> str:
         """
         Returns the proper qemu command based on virtualization.
+        This command assumes qcow2 img has no alpine installed yet
 
         Args:
             qcow2_file (Path): 
             alpine_iso (Path): 
             virtualization (str): macos or linux
+            loadvm (bool): loads self.snapshot_name if True, otherwise boot from qcow2 img
 
         Returns:
             str: The qemu command
         """
-        accel = "kvm:tcg" if virtualization == "macos" else "tcg"
+        accel = "tcg" if virtualization == "macos" else "kvm:tcg"
         cpu = "max" if virtualization == "macos" else "host"
         kvm = "-enable-kvm" if virtualization == "linux" else ""
+        loadvmflag = f"-loadvm {self.snapshot_name}" if loadvm else "" 
 
         cmd = (
             "qemu-system-x86_64",
             f"-machine accel={accel},usb=off",
             f"-cpu {cpu}",
-            "-m 512M",
-            f"-cdrom {alpine_iso}",
-            f"-hda {qcow2_file}",
+            f"-smp {self.cpu_count}"
+            f"-m {self.memory_allocated}M",
+            f"-hda {self.img_path}",
             "-netdev user,id=net0,hostfwd=tcp::2222-:22",
             "-device virtio-net,netdev=net0",
             "-nographic",
-            kvm
+            kvm,
+            loadvmflag
         )
         
         return " ".join(filter(None, cmd))
     
-
-    def _validate_setup_args(self, qcow2_file: Path, alpine_iso: Path, virtualization: str) -> None:
-        if not qcow2_file.exists():
-            raise ValueError(f"Provided qcow2 file '{qcow2_file}' does not exist.")
-        
-        if not alpine_iso.exists():
-            raise ValueError(f"{alpine_iso} does not exist.")
-        
-        if virtualization not in ["macos", "linux"]:
-            raise ValueError(f"Provided invalid virtualization '{virtualization}'. Expects 'macos' or 'linux'.")
-    
-    @staticmethod
-    def _get_alpine_setup_prompts() -> list[tuple[str, str]]:
-        return [
-            ("localhost login:", "root"),
-            ("Password:", ""),  # empty password
-
-            # Start of setup
-            ("localhost:~#", "setup-alpine"),
-            
-            # Hostname
-            ("Enter system hostname", ""),
-            # Network interface
-            ("Which one do you want to initialize?", ""),
-            ("Ip address for eth0?", ""),
-
-            # Root password
-            ("New password:", ""),
-            ("Retype password:", ""),
-
-            # Timezone
-            ("Which timezone are you in?", "Asia"),
-            ("Which timezone are you in", "Manila"),
-
-            # Proxy. default: none 
-            ("HTTP/FTP proxy URL?", ""),
-            # NTP. default: busybox
-            ("Which NTP client to run?", ""),
-            # Mirrors 
-            ("Enter mirror number", ""),
-            # User setup. default: no
-            ("Setup a user?", ""),
-            # SSH. default: openssh
-            ("Which ssh server?", ""),
-            # default: prohibit-password
-            ("Allow root ssh login?", ""),
-            # default: none
-            ("Enter ssh key", ""),
-            
-            # Disk setup
-            ("Which disk(s) would you like to use?", "sda"),
-            ("How would you like to use it?", "sys"),
-            ("Erase the above disk", "y"),
-        ]
--- test/test_qemu_controller.py
+++ test/test_qemu_controller.py
@@ -1,15 +1,65 @@
+from pytest import fail
 from pathlib import Path
-from worker_node.core.qemu_controller import QemuController
+from src.worker_node.core.qemu_controller import QemuController
 
 # tmp_path is a built in fixture by pytest that provides temproray path
 # this path gets automatically cleaned up after running the test
 
-def test_create_qcow2_img(tmp_path: Path) -> None:
-	img_path = tmp_path / "test_disk.qcow2"
+alpine_iso = Path("/Users/luis/netes/x86/alpine-virt-3.22.1-x86_64.iso")
 
-	QemuController.create_qcow2_img(img_path, 64)
+def test_create_qcow2_img(tmp_path: Path):
+	img_path = tmp_path / "test_disk.qcow2"
+	qemu_controller = QemuController(img_path)
+
+	qemu_controller.create_qcow2_img(img_path, 64)
 
 	assert img_path.exists()
 
 
-def
+def test_get_qemu_cmd(tmp_path: Path):
+	q_cont = QemuController(tmp_path)
+
+	# For macos
+	cmd = q_cont._get_qemu_cmd(alpine_iso=alpine_iso, virtualization="macos") # type: ignore
+	expected = (
+            "qemu-system-x86_64",
+            "-machine accel=tcg,usb=off",
+            "-cpu max",
+            "-m 512M",
+            f"-cdrom {alpine_iso}",
+            f"-hda {tmp_path}",
+            "-netdev user,id=net0,hostfwd=tcp::2222-:22",
+            "-device virtio-net,netdev=net0",
+            "-nographic"
+        )
+	
+	assert cmd == " ".join(filter(None, expected))
+
+	# For linux
+	cmd = q_cont._get_qemu_cmd(alpine_iso=alpine_iso, virtualization="linux") # type: ignore
+	expected = (
+            "qemu-system-x86_64",
+            "-machine accel=kvm:tcg,usb=off",
+            "-cpu host",
+            "-m 512M",
+            f"-cdrom {alpine_iso}",
+            f"-hda {tmp_path}",
+            "-netdev user,id=net0,hostfwd=tcp::2222-:22",
+            "-device virtio-net,netdev=net0",
+            "-nographic",
+			"-enable-kvm"
+        )
+	
+	assert cmd == " ".join(filter(None, expected))
+
+def test_setup_alpine(tmp_path: Path):
+	img_path = tmp_path / "test_disk.qcow2"
+	qemu_cont = QemuController(img_path)
+
+	qemu_cont.create_qcow2_img(img_path, 4000)
+
+	try:
+		qemu_cont.setup_alpine_linux(alpine_iso, "macos")
+	except Exception as e:
+		fail(f"Setup_alpine_linux failed with exception: {e}") 
+	
